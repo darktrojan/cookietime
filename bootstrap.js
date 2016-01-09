2ee14dd3f3e44c1972531c21e40704e8c6747407
Geoff Lankow <geoff@darktrojan.net>
2016-01-09 21:59:54 +1300

    JSCS bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,58 +1,58 @@
 /* globals ADDON_INSTALL, APP_SHUTDOWN */
-const ADDON_ID = "cookietime@darktrojan.net";
-const ADDON_PREF_PAGE = "addons://detail/cookietime@darktrojan.net/preferences";
+var ADDON_ID = 'cookietime@darktrojan.net';
+var ADDON_PREF_PAGE = 'addons://detail/cookietime@darktrojan.net/preferences';
 
-const BROWSER_WINDOW_TYPE = "navigator:browser";
-const BROWSER_WINDOW_URL = "chrome://browser/content/browser.xul";
+var BROWSER_WINDOW_TYPE = 'navigator:browser';
+var BROWSER_WINDOW_URL = 'chrome://browser/content/browser.xul';
 
-const PREF_BRANCH = "extensions.cookietime.";
-const PREF_DELETE_EXPIRED_COUNT = "deleteExpired.count";
-const PREF_DELETE_EXPIRED_ENABLED = "deleteExpired.enabled";
-const PREF_DELETE_UNUSED_COUNT = "deleteUnused.count";
-const PREF_DELETE_UNUSED_DAYS = "deleteUnused.days";
-const PREF_EXPIRE_COUNT = "expire.count";
-const PREF_EXPIRE_DAYS = "expire.days";
-const PREF_IDLE_ENABLED = "idle.enabled";
-const PREF_IDLE_LASTRAN = "idle.lastran";
+var PREF_BRANCH = 'extensions.cookietime.';
+var PREF_DELETE_EXPIRED_COUNT = 'deleteExpired.count';
+var PREF_DELETE_EXPIRED_ENABLED = 'deleteExpired.enabled';
+var PREF_DELETE_UNUSED_COUNT = 'deleteUnused.count';
+var PREF_DELETE_UNUSED_DAYS = 'deleteUnused.days';
+var PREF_EXPIRE_COUNT = 'expire.count';
+var PREF_EXPIRE_DAYS = 'expire.days';
+var PREF_IDLE_ENABLED = 'idle.enabled';
+var PREF_IDLE_LASTRAN = 'idle.lastran';
 
-const SECONDS_IN_DAY = 86400;
-const MS_IN_SECOND = 1000;
-const US_IN_SECOND = 1000000;
+var SECONDS_IN_DAY = 86400;
+var MS_IN_SECOND = 1000;
+var US_IN_SECOND = 1000000;
 
-const DAY_INCREMENTS_SHORT = [7, 14, 30, 60, 91, 182, 273, 365, 547];
-const DAY_INCREMENTS_LONG = [7, 14, 30, 60, 91, 182, 273, 365, 547, 730, 1095, 1825];
+var DAY_INCREMENTS_SHORT = [7, 14, 30, 60, 91, 182, 273, 365, 547];
+var DAY_INCREMENTS_LONG = [7, 14, 30, 60, 91, 182, 273, 365, 547, 730, 1095, 1825];
 
 /* globals Components, Services, Sqlite, Task, PluralForm, XPCOMUtils */
-Components.utils.import("resource://gre/modules/Promise.jsm");
-Components.utils.import("resource://gre/modules/Services.jsm");
-Components.utils.import("resource://gre/modules/Sqlite.jsm");
-Components.utils.import("resource://gre/modules/Task.jsm");
-Components.utils.import("resource://gre/modules/PluralForm.jsm");
-Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import('resource://gre/modules/Promise.jsm');
+Components.utils.import('resource://gre/modules/Services.jsm');
+Components.utils.import('resource://gre/modules/Sqlite.jsm');
+Components.utils.import('resource://gre/modules/Task.jsm');
+Components.utils.import('resource://gre/modules/PluralForm.jsm');
+Components.utils.import('resource://gre/modules/XPCOMUtils.jsm');
 
 /* globals strings, getPlural */
-XPCOMUtils.defineLazyGetter(this, "strings", function() {
-	return Services.strings.createBundle("chrome://cookietime/locale/cookietime.properties");
+XPCOMUtils.defineLazyGetter(this, 'strings', function() {
+	return Services.strings.createBundle('chrome://cookietime/locale/cookietime.properties');
 });
-XPCOMUtils.defineLazyGetter(this, "getPlural", function() {
-	return PluralForm.makeGetter(strings.GetStringFromName("pluralForm"))[0];
+XPCOMUtils.defineLazyGetter(this, 'getPlural', function() {
+	return PluralForm.makeGetter(strings.GetStringFromName('pluralForm'))[0];
 });
 
-let optionsObserver = {
+var optionsObserver = {
 	observe: function(aDocument, aTopic, aData) {
-		switch(aTopic) {
-		case "addon-options-displayed":
+		switch (aTopic) {
+		case 'addon-options-displayed':
 			if (aData != ADDON_ID) {
 				return;
 			}
 
 			this.updateAffectedCounts(aDocument);
 
-			aDocument.getElementById("cookietime.runnow").addEventListener("command", () => {
+			aDocument.getElementById('cookietime.runnow').addEventListener('command', () => {
 				autoRunQueries().then(count => {
 					let deleted = count.deleteExpired + count.deleteUnused;
 					let modified = count.expire;
-					aDocument.defaultView.alert(strings.formatStringFromName("message", [formatPlural(deleted), formatPlural(modified)], 2));
+					aDocument.defaultView.alert(strings.formatStringFromName('message', [formatPlural(deleted), formatPlural(modified)], 2));
 					this.updateAffectedCounts(aDocument);
 				});
 			});
@@ -61,42 +61,42 @@ let optionsObserver = {
 
 	updateAffectedCounts: function(aDocument) {
 		countQueries().then(result => {
-			aDocument.getElementById("cookietime.deleteExpired.enabled").setAttribute("desc", formatPlural(result.deleteExpired));
-			let deleteUnusedItem = aDocument.getElementById("cookietime.deleteUnused.days").querySelector("menuitem");
+			aDocument.getElementById('cookietime.deleteExpired.enabled').setAttribute('desc', formatPlural(result.deleteExpired));
+			let deleteUnusedItem = aDocument.getElementById('cookietime.deleteUnused.days').querySelector('menuitem');
 			for (let day in result.deleteUnused) {
-				deleteUnusedItem.setAttribute("description", formatPlural(result.deleteUnused[day]));
+				deleteUnusedItem.setAttribute('description', formatPlural(result.deleteUnused[day]));
 				deleteUnusedItem = deleteUnusedItem.nextElementSibling;
 			}
-			let expireItem = aDocument.getElementById("cookietime.expire.days").querySelector("menuitem");
+			let expireItem = aDocument.getElementById('cookietime.expire.days').querySelector('menuitem');
 			for (let day in result.expire) {
-				expireItem.setAttribute("description", formatPlural(result.expire[day]));
+				expireItem.setAttribute('description', formatPlural(result.expire[day]));
 				expireItem = expireItem.nextElementSibling;
 			}
 		});
 	}
 };
 
-let idleObserver = {
+var idleObserver = {
 	observe: function(aSubject, aTopic) {
-		switch(aTopic) {
-		case "cookietime-idle":
-		case "idle-daily":
+		switch (aTopic) {
+		case 'cookietime-idle':
+		case 'idle-daily':
 			if (Services.prefs.getBoolPref(PREF_BRANCH + PREF_IDLE_ENABLED)) {
 				autoRunQueries().then(count => {
 					let deleted = count.deleteExpired + count.deleteUnused;
 					let modified = count.expire;
 					Services.prefs.setIntPref(PREF_BRANCH + PREF_IDLE_LASTRAN, Math.floor(Date.now() / MS_IN_SECOND));
-					Services.console.logStringMessage(aTopic + ": " + strings.formatStringFromName("message", [formatPlural(deleted), formatPlural(modified)], 2));
+					Services.console.logStringMessage(aTopic + ': ' + strings.formatStringFromName('message', [formatPlural(deleted), formatPlural(modified)], 2));
 				});
-			} else if (aTopic == "cookietime-idle") {
-				Services.console.logStringMessage(aTopic + ": " + strings.GetStringFromName("idle-disabled"));
+			} else if (aTopic == 'cookietime-idle') {
+				Services.console.logStringMessage(aTopic + ': ' + strings.GetStringFromName('idle-disabled'));
 			}
 			break;
 		}
 	}
 };
 
-let windowHandler = {
+var windowHandler = {
 	load: function() {
 		this.enumerateWindows(this.paint);
 		Services.ww.registerNotification(this);
@@ -114,9 +114,9 @@ let windowHandler = {
 		}
 	},
 	observe: function(aSubject, aTopic) {
-		if (aTopic == "domwindowopened") {
-			aSubject.addEventListener("load", function onload() {
-				aSubject.removeEventListener("load", onload);
+		if (aTopic == 'domwindowopened') {
+			aSubject.addEventListener('load', function onload() {
+				aSubject.removeEventListener('load', onload);
 				windowHandler.paint(aSubject);
 			});
 		} else {
@@ -129,15 +129,15 @@ let windowHandler = {
 		}
 
 		let document = aWindow.document;
-		let menuitem = document.createElement("menuitem");
-		menuitem.id = "tools-cookietime";
-		menuitem.className = "menuitem-iconic";
-		menuitem.setAttribute("label", strings.GetStringFromName("options.label"));
-		menuitem.addEventListener("command", function() {
+		let menuitem = document.createElement('menuitem');
+		menuitem.id = 'tools-cookietime';
+		menuitem.className = 'menuitem-iconic';
+		menuitem.setAttribute('label', strings.GetStringFromName('options.label'));
+		menuitem.addEventListener('command', function() {
 			windowHandler.openPreferences();
 		});
 
-		let toolsPopup = document.getElementById("menu_ToolsPopup");
+		let toolsPopup = document.getElementById('menu_ToolsPopup');
 		toolsPopup.appendChild(menuitem);
 	},
 	unpaint: function(aWindow) {
@@ -146,7 +146,7 @@ let windowHandler = {
 		}
 
 		let document = aWindow.document;
-		document.getElementById("tools-cookietime").remove();
+		document.getElementById('tools-cookietime').remove();
 	},
 	showConfigMessage: function(aIcon) {
 		let recentWindow = Services.wm.getMostRecentWindow(BROWSER_WINDOW_TYPE);
@@ -154,15 +154,15 @@ let windowHandler = {
 			return;
 		}
 
-		let label = strings.GetStringFromName("notification.label");
-		let value = "notify-cookietime";
+		let label = strings.GetStringFromName('notification.label');
+		let value = 'notify-cookietime';
 		let buttons = [{
-			label: strings.GetStringFromName("notification-button.label"),
-			accessKey: strings.GetStringFromName("notification-button.accesskey"),
+			label: strings.GetStringFromName('notification-button.label'),
+			accessKey: strings.GetStringFromName('notification-button.accesskey'),
 			callback: this.openPreferences
 		}];
 
-		let notifyBox = recentWindow.document.getElementById("global-notificationbox");
+		let notifyBox = recentWindow.document.getElementById('global-notificationbox');
 		notifyBox.appendNotification(label, value, aIcon, notifyBox.PRIORITY_INFO_LOW, buttons);
 	},
 	openPreferences: function() {
@@ -188,14 +188,14 @@ function startup(aParams, aReason) {
 	defaultPrefs.setIntPref(PREF_EXPIRE_DAYS, 90);
 	defaultPrefs.setBoolPref(PREF_IDLE_ENABLED, false);
 
-	Services.obs.addObserver(optionsObserver, "addon-options-displayed", false);
-	Services.obs.addObserver(idleObserver, "idle-daily", false);
-	Services.obs.addObserver(idleObserver, "cookietime-idle", false);
+	Services.obs.addObserver(optionsObserver, 'addon-options-displayed', false);
+	Services.obs.addObserver(idleObserver, 'idle-daily', false);
+	Services.obs.addObserver(idleObserver, 'cookietime-idle', false);
 
 	windowHandler.load();
 
 	if (aReason == ADDON_INSTALL) {
-		windowHandler.showConfigMessage(aParams.resourceURI.spec + "icon.png");
+		windowHandler.showConfigMessage(aParams.resourceURI.spec + 'icon.png');
 	}
 }
 
@@ -206,9 +206,9 @@ function shutdown(aParams, aReason) {
 		return;
 	}
 
-	Services.obs.removeObserver(optionsObserver, "addon-options-displayed");
-	Services.obs.removeObserver(idleObserver, "idle-daily", false);
-	Services.obs.removeObserver(idleObserver, "cookietime-idle", false);
+	Services.obs.removeObserver(optionsObserver, 'addon-options-displayed');
+	Services.obs.removeObserver(idleObserver, 'idle-daily', false);
+	Services.obs.removeObserver(idleObserver, 'cookietime-idle', false);
 }
 
 function autoRunQueries() {
@@ -248,28 +248,25 @@ function autoRunQueries() {
 function countQueries() {
 	let deferred = Promise.defer();
 	Task.spawn(function*() {
-		let connection = yield Sqlite.openConnection({ path: "cookies.sqlite" });
+		let connection = yield Sqlite.openConnection({ path: 'cookies.sqlite' });
 		try {
 			let results = {
 				deleteExpired: 0,
 				deleteUnused: {},
 				expire: {}
-			};
-			{
-				let sql = "SELECT COUNT(*) FROM moz_cookies WHERE expiry < strftime('%s', 'now')";
+			}; {
+				let sql = 'SELECT COUNT(*) FROM moz_cookies WHERE expiry < strftime(\'%s\', \'now\')';
 				let result = yield connection.execute(sql);
 				results.deleteExpired = result[0].getResultByIndex(0);
-			}
-			{
-				let sql = "SELECT COUNT(*) FROM moz_cookies WHERE lastAccessed < strftime('%s000000', 'now') - :us";
+			} {
+				let sql = 'SELECT COUNT(*) FROM moz_cookies WHERE lastAccessed < strftime(\'%s000000\', \'now\') - :us';
 				for (let days of DAY_INCREMENTS_SHORT) {
 					let params = { us: days * SECONDS_IN_DAY * US_IN_SECOND };
 					let result = yield connection.executeCached(sql, params);
 					results.deleteUnused[days] = yield result[0].getResultByIndex(0);
 				}
-			}
-			{
-				let sql = "SELECT COUNT(*) FROM moz_cookies WHERE expiry > strftime('%s', 'now') + :s";
+			} {
+				let sql = 'SELECT COUNT(*) FROM moz_cookies WHERE expiry > strftime(\'%s\', \'now\') + :s';
 				for (let days of DAY_INCREMENTS_LONG) {
 					let params = { s: days * SECONDS_IN_DAY };
 					let result = yield connection.executeCached(sql, params);
@@ -289,19 +286,19 @@ function countQueries() {
 function runQueries(aDeleteExpired, aDeleteUnusedDays, aExpireDays) {
 	let deferred = Promise.defer();
 	Task.spawn(function*() {
-		let connection = yield Sqlite.openConnection({ path: "cookies.sqlite" });
+		let connection = yield Sqlite.openConnection({ path: 'cookies.sqlite' });
 		try {
 			if (aDeleteExpired) {
-				let sql = "DELETE FROM moz_cookies WHERE expiry < strftime('%s', 'now')";
+				let sql = 'DELETE FROM moz_cookies WHERE expiry < strftime(\'%s\', \'now\')';
 				yield connection.execute(sql);
 			}
 			if (aDeleteUnusedDays > 0) {
-				let sql = "DELETE FROM moz_cookies WHERE lastAccessed < strftime('%s000000', 'now') - :us";
+				let sql = 'DELETE FROM moz_cookies WHERE lastAccessed < strftime(\'%s000000\', \'now\') - :us';
 				let params = { us: aDeleteUnusedDays * SECONDS_IN_DAY * US_IN_SECOND };
 				yield connection.execute(sql, params);
 			}
 			if (aExpireDays > 0) {
-				let sql = "UPDATE moz_cookies SET expiry = MIN(strftime('%s', 'now') + :s, expiry)";
+				let sql = 'UPDATE moz_cookies SET expiry = MIN(strftime(\'%s\', \'now\') + :s, expiry)';
 				let params = { s: aExpireDays * SECONDS_IN_DAY };
 				yield connection.execute(sql, params);
 			}
@@ -320,7 +317,7 @@ function increaseCount(aPref, aCount) {
 	Services.prefs.setIntPref(PREF_BRANCH + aPref, count + aCount);
 }
 
-function formatPlural(aCount, aKey="cookieCount") {
+function formatPlural(aCount, aKey='cookieCount') {
 	let formats = strings.GetStringFromName(aKey);
-	return getPlural(aCount, formats).replace("%S", aCount);
+	return getPlural(aCount, formats).replace('%S', aCount);
 }
